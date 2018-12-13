ee4dc8b6418d3e2f02b9cebc2b78027a03f34453
Saulius444 <Saulius444@users.noreply.github.com>
2018-12-13 15:04:31 +0200

    End of Section 12

--- DatingApp/DatingApp-SPA/src/app/home/home.component.ts
+++ DatingApp/DatingApp-SPA/src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { AlertifyService } from '../_services/alertify.service';
 
 @Component({
   selector: 'app-home',
@@ -9,7 +10,14 @@ import { HttpClient } from '@angular/common/http';
 export class HomeComponent implements OnInit {
   registerMode = false;
 
-  constructor(private http: HttpClient) { }
+  // learnMore
+  bodyPart = ['face', 'foot', 'nose', 'hand', 'head'];
+  adjective = ['hairy and', 'extremely', 'insultingly', 'astonishingly'];
+  adjectiveTwo = ['stupid', 'gigantic', 'fat', 'horrid', 'scary'];
+  animal = ['baboon', 'sasquatch', 'sloth', 'naked cat', 'warthog'];
+  // end learnMore
+
+  constructor(private http: HttpClient, private alertify: AlertifyService) { }
 
   ngOnInit() {
   }
@@ -17,6 +25,22 @@ export class HomeComponent implements OnInit {
   registerToggle() {
     this.registerMode = true;
   }
+
+  // learnMore
+  randomInsult() {
+    const rand1 = this.bodyPart[Math.floor(Math.random() * this.bodyPart.length)];
+    const rand2 = this.adjective[Math.floor(Math.random() * this.adjective.length)];
+    const rand3 = this.adjectiveTwo[Math.floor(Math.random() * this.adjectiveTwo.length)];
+    const rand4 = this.animal[Math.floor(Math.random() * this.animal.length)];
+
+      return 'Your' + ' ' + rand1 + ' ' + 'is more' + ' ' + rand2 + ' '
+        + rand3 + ' ' + 'than a' + ' ' + rand4 + '\'s' + ' ' + rand1 + '.';
+  }
+
+  learnMore() {
+    this.alertify.success(this.randomInsult());
+  }
+  // end learnMore
 
   cancelRegisterMode(registerMode: boolean) {
     this.registerMode = registerMode;
