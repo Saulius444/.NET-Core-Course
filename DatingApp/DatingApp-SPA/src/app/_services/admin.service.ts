f75484d723a3c62385612f62f1bfe62589917772
Saulius444 <Saulius444@users.noreply.github.com>
2019-02-26 16:01:09 +0200

    End of Course

--- DatingApp/DatingApp-SPA/src/app/_services/admin.service.ts
+++ DatingApp/DatingApp-SPA/src/app/_services/admin.service.ts
@@ -18,4 +18,16 @@ constructor(private http: HttpClient) { }
   updateUserRoles(user: User, roles: {}) {
     return this.http.post(this.baseUrl + 'admin/editRoles/' + user.userName, roles);
   }
+
+  getPhotoForApproval() {
+    return this.http.get(this.baseUrl + 'admin/photosForModeration');
+  }
+
+  approvePhoto(photoId) {
+    return this.http.post(this.baseUrl + 'admin/approvePhoto/' + photoId, {});
+  }
+
+  rejectPhoto(photoId) {
+    return this.http.post(this.baseUrl + 'admin/rejectPhoto/' + photoId, {});
+  }
 }
